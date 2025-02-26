a5e9d65969e177481510404d03b8d7f1c9af4c00
David Bristol <16607760+davidbristol@users.noreply.github.com>
2025-02-26 23:35:06 +0000

    Update app name to reflect functionality
    Changed the default application name in the configuration file to "AWS S3 Presigned Upload URL Creator." This improves clarity about the app's purpose and functionality.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'AWS S3 Presigned Upload URL Creator'),
 
     /*
     |--------------------------------------------------------------------------
