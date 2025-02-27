1ed4e721de138aacf25a411020a99c29bf27204c
David Bristol <16607760+davidbristol@users.noreply.github.com>
2025-02-27 10:38:16 +0000

    Update S3 signed URL command with validation and expiry
    Refined bucket name validation logic with prohibited prefixes/suffixes and stricter rules. Added customizable URL expiry duration (up to 168 hours). Improved code structure and user feedback for better clarity and functionality.

--- app/Console/Commands/AwsS3SignedUploadUrl.php
+++ app/Console/Commands/AwsS3SignedUploadUrl.php
@@ -4,14 +4,28 @@ namespace App\Console\Commands;
 
 use Aws\S3\S3Client;
 use Closure;
+use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use RuntimeException;
 
 final class AwsS3SignedUploadUrl extends Command
 {
-    protected $signature = 'aws:s3-signed-upload-url {bucket_name? : The S3 bucket name} {file? : The S3 file key to create the presigned URL for} {--r|region= : The AWS Region to use (default: us-east-1)} {--k|key= : The AWS Access Key ID to use} {--s|secret= : The AWS Secret Access Key to use}';
+    const array PROHIBITED_PREFIXES = [
+        'xn--',
+        'sthree-',
+        'amzn-s3-demo-',
+    ];
+    const array PROHIBITED_SUFFIXES = [
+        '-s3alias',
+        '--ol-s3',
+        '.mrap',
+        '--x-s3',
+    ];
+
+    protected $signature = 'aws:s3-signed-upload-url {bucket_name : The S3 bucket name} {file? : The S3 file key to create the presigned URL for} {--r|region= : The AWS Region to use (default: us-east-1)} {--k|key= : The AWS Access Key ID to use} {--s|secret= : The AWS Secret Access Key to use} {--hours=24 : The number of hours the presigned URL should be valid for}';
 
     protected $description = 'Command description';
 
@@ -24,20 +38,21 @@ final class AwsS3SignedUploadUrl extends Command
 
     private function buildAwsClient(): void
     {
-        if (empty($this->awsClient)) {
-            $this->awsClient = new S3Client([
-                'version' => 'latest',
-                'region' => $this->getAwsRegion(),
-                'credentials' => [
-                    'key' => $this->getAwsAccessKeyId(),
-                    'secret' => $this->getAwsSecretKey(),
-                ],
-            ]);
-        }
+        $this->awsClient = new S3Client([
+            'version' => 'latest',
+            'region' => $this->getAwsRegion(),
+            'credentials' => [
+                'key' => $this->getAwsAccessKeyId(),
+                'secret' => $this->getAwsSecretKey(),
+            ],
+        ]);
     }
 
     public function handle(): int
     {
+        $this->output->title('Generate an AWS S3 presigned upload URL');
+        $this->output->info("Current Datetime: " . Carbon::now()->toDateTimeString());
+
         try {
             $this->buildAwsClient();
         } catch (RuntimeException $exception) {
@@ -46,12 +61,16 @@ final class AwsS3SignedUploadUrl extends Command
             return self::FAILURE;
         }
 
+        $bucketName = $this->getBucketName();
+        $file = $this->getFile();
+        $expiresAfter = $this->getExpiresAfter();
+
         try {
             $command = $this->awsClient->getCommand('PutObject', [
-                'Bucket' => $this->getBucketName(),
-                'Key' => $this->getFile(), // Generate a unique filename
+                'Bucket' => $bucketName,
+                'Key' => $file,
             ]);
-        } catch (\Exception $exception) {
+        } catch (Exception $exception) {
             $this->error($exception->getMessage());
 
             return self::FAILURE;
@@ -68,12 +87,18 @@ final class AwsS3SignedUploadUrl extends Command
 
     private function getFile(): string
     {
-        return $this->argument('file') ?? $this->ask('Enter the S3 file to create presigned upload URL for');
+        $file = $this->argument('file') ?? $this->ask('Enter the S3 file to create presigned upload URL for');
+
+        if (!empty($file)) {
+            return $file;
+        }
+
+        return 'todo';
     }
 
     private function getBucketName(): string
     {
-        $bucketName = $this->argument('bucket_name') ?? $this->ask('Enter the S3 bucket name to create presigned upload URL for');
+        $bucketName = $this->argument('bucket_name');
 
         $validator = Validator::make(
             ['bucket_name' => $bucketName],
@@ -90,23 +115,23 @@ final class AwsS3SignedUploadUrl extends Command
                             $fail('Bucket name cannot be formatted as an IP address.');
                         }
 
-                        // Check for prohibited prefixes
-                        if (str_starts_with($value, 'xn--') || str_starts_with($value, 'sthree-')) {
-                            $fail('Bucket name cannot start with "xn--" or "sthree-".');
-                        }
-
-                        // Check for prohibited suffix
-                        if (str_ends_with($value, '-s3alias')) {
-                            $fail('Bucket name cannot end with "-s3alias".');
+                        // Check for prohibited prefixes and suffixes
+                        if (Str::of($value)->startsWith(self::PROHIBITED_PREFIXES) || Str::of($value)->endsWith(self::PROHIBITED_SUFFIXES)) {
+                            $fail(
+                                sprintf('Bucket name cannot start with %s or end with %s.',
+                                    Str::replaceLast(', ', ', or ', implode(', ', self::PROHIBITED_PREFIXES)),
+                                    Str::replaceLast(', ', ', or ', implode(', ', self::PROHIBITED_SUFFIXES))
+                                )
+                            );
                         }
 
                         // Check for adjacent periods
-                        if (str_contains($value, '..')) {
+                        if (Str::of($value)->contains('..')) {
                             $fail('Bucket name cannot contain adjacent periods.');
                         }
 
                         // Check for dashes next to periods
-                        if (str_contains($value, '-.') || str_contains($value, '.-')) {
+                        if (Str::of($value)->contains(['-.', '.-'])) {
                             $fail('Bucket name cannot contain dashes next to periods.');
                         }
 
@@ -126,12 +151,22 @@ final class AwsS3SignedUploadUrl extends Command
                 $this->line("  - $error");
             }
 
-            throw new RuntimeException('Invalid S3 bucket name.');
+            throw new RuntimeException("Invalid S3 bucket name: $bucketName.");
         }
 
         return $bucketName;
     }
 
+    /**
+     * Retrieve the AWS Region.
+     *
+     * This method retrieves the AWS Region from either the --region
+     * command-line option or the AWS_REGION environment variable. It validates
+     * the region against the available AWS regions data, and throws a runtime
+     * exception if the region is invalid or unspecified.
+     *
+     * @throws RuntimeException If the AWS Region is missing or invalid.
+     */
     private function getAwsRegion(): string
     {
         $endpointsFile = base_path('/vendor/aws/aws-sdk-php/src/data/endpoints.json.php');
@@ -155,17 +190,6 @@ final class AwsS3SignedUploadUrl extends Command
         return $region;
     }
 
-
-    /**
-     * Retrieve the AWS Region.
-     *
-     * This method retrieves the AWS Region from either the --region
-     * command-line option or the AWS_REGION environment variable. It validates
-     * the region against the available AWS regions data, and throws a runtime
-     * exception if the region is invalid or unspecified.
-     *
-     * @throws RuntimeException If the AWS Region is missing or invalid.
-     */
     private function getAwsAccessKeyId(): string
     {
         $key = $this->option('key') ?? env('AWS_ACCESS_KEY_ID');
@@ -209,5 +233,18 @@ final class AwsS3SignedUploadUrl extends Command
         $this->info(sprintf("Using AWS Secret Access Key: %s", Str::of($secret)->mask('*', 4, 32)));
 
         return $secret;
+    }
+
+    private function getExpiresAfter(): Carbon
+    {
+        $hours = $this->option('hours') ?? $this->ask('Enter the number of hours for the URL to expire after (max 168 hours (7 days))');
+
+        if (!is_numeric($hours) || $hours <= 0 || $hours > 168) {
+            throw new RuntimeException('Invalid hours value. Please specify a number between 1 and 168.');
+        }
+
+        $this->info(sprintf("URL will expire after %s hours.", $hours));
+
+        return Carbon::now()->addHours($hours);
     }
 }
